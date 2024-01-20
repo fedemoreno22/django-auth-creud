171fff2723d52aafe0d771063abd604a050f92e9
Federico Moreno <FMORENO@loteriadelaciudad.gob.ar>
2024-01-20 09:48:52 -0300

    menu responsive

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -82,12 +82,19 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres@localhost/postgres',
-        conn_max_age=600
-    )
+     'default': dj_database_url.config(
+         default='postgresql://postgres@localhost/postgres',
+         conn_max_age=600
+     )
 
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
